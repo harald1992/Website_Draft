f535669bdf4d18a1f79dda66901267d4aaf363c2
Harald <37929926+harald1992@users.noreply.github.com>
2019-01-07 16:17:18 +0100

    Initial Commit

---
+++ Website_Draft/Controllers/CvBuilderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Website_Draft.Controllers
+{
+    public class CvBuilderController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
