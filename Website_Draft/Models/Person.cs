efbaa17b5f4b9eb04948ab815667411f10e61ea9
Harald <37929926+harald1992@users.noreply.github.com>
2019-01-07 17:10:47 +0100

    Start for admin index

---
+++ Website_Draft/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website_Draft.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+    }
+}
